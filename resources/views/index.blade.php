a5b9ad7ac5758f3fa0fd6dbe91fa19f85135f0a9
Mihir Patel <rs.mihirpatel19@gmail.com>
2025-03-26 13:19:38 +0530

    changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,43 +9,38 @@
 
 </head>
 
-<body>
-    <div class="container">
-        <div class="row mt-5 align-items-center">
-            <div class="col-md-6 text-center">
-                <img src="{{ asset('assets/attachment/tata-ipl-logo-png_seeklogo-531750.png') }}" alt="IPL Logo"
-                    class="img-fluid" style="max-width: 70%; height: auto;">
-            </div>
-            <div class="col-md-6">
+<body
+    style="background-image: url('{{ asset('assets/attachment/bg-img.jpg') }}'); height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="card p-4 shadow" style="width: 400px;">
+            <div class="card-body">
+                <div class="text-center mb-3">
+                    <img src="{{ asset('assets/attachment/tata-ipl-logo-png_seeklogo-531750.png') }}" alt="IPL Logo"
+                        class="img-fluid" style="max-width: 70%; height: auto;">
+                </div>
                 {!! Form::open(['route' => 'user.login', 'method' => 'post', 'id' => 'loginForm']) !!}
-                <div class="col-md-8">
-                    <div class="form-group">
-                        {!! Form::label('userName', 'Name', ['class' => 'required']) !!}
-                        {!! Form::text('userName', '', [
-                            'class' => 'form-control form-control-sm required',
-                            'placeholder' => 'Enter your name',
-                            'id' => 'userName',
-                        ]) !!}
-                    </div>
+                <div class="form-group">
+                    {!! Form::label('userName', 'Name', ['class' => 'required']) !!}
+                    {!! Form::text('userName', '', [
+                        'class' => 'form-control form-control-sm required',
+                        'placeholder' => 'Enter your name',
+                        'id' => 'userName',
+                    ]) !!}
                 </div>
-                <div class="col-md-8">
-                    <div class="form-group">
-                        {!! Form::label('password', 'Password', ['class' => 'required']) !!}
-                        {!! Form::password('password', [
-                            'class' => 'form-control form-control-sm required',
-                            'placeholder' => 'Enter your password',
-                            'id' => 'password',
-                        ]) !!}
-                    </div>
+                <div class="form-group">
+                    {!! Form::label('password', 'Password', ['class' => 'required']) !!}
+                    {!! Form::password('password', [
+                        'class' => 'form-control form-control-sm required',
+                        'placeholder' => 'Enter your password',
+                        'id' => 'password',
+                    ]) !!}
                 </div>
-                <div class="col-md-6">
-                    <div style="margin-bottom: 20px;">
-                        @if (session('error'))
-                            <div class="d-block">
-                                {{ session('error') }}
-                            </div>
-                        @endif
+                @if (session('error'))
+                    <div class="text-danger mb-2">
+                        {{ session('error') }}
                     </div>
+                @endif
+                <div>
                     <button type="submit" class="btn btn-outline-primary btn-sm">Submit</button>
                     <button type="reset" class="btn btn-outline-info btn-sm">Cancel</button>
                 </div>
@@ -53,7 +48,5 @@
             </div>
         </div>
     </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
-
 </html>
